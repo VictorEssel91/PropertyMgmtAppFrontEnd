79f801fd2c59004475ae71d91334dd3232864695
unknown <victoressel91@gmail.com>
2023-06-14 03:13:41 -0400

    connect to backend CRUD operations

--- PropertyMgmt-UI/src/app/property/property.component.ts
+++ PropertyMgmt-UI/src/app/property/property.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-property',
@@ -8,14 +9,107 @@ import { Component } from '@angular/core';
 export class PropertyComponent {
 
 PropertyArray : any[] = [];
+isResultLoaded= false;
+isUpdateFormActive = false;
 
 
 propertyAddress: string = "";
 propertyRentedStatus: string = "";
 propertyContactNumber: number =0;
+currentPropertyId = "";
 
-propertyId = "";
+constructor (private http: HttpClient)
+{
+  this.getAllProperties();
+}
 
+ getAllProperties()
+  {
+
+    this.http.get("http://localhost:8082/api/v1/property/getAllProperties")
+
+    .subscribe((resultData: any)=>
+    {
+        this.isResultLoaded = true;
+        console.log(resultData);
+        this.PropertyArray = resultData;
+    });
+  }
+  register()
+  {
+
+    let bodyData = {
+      "propertyAddress" : this.propertyAddress,
+      "propertyRentedStatus" : this.propertyRentedStatus,
+      "propertyContactNumber" : this.propertyContactNumber
+    };
+    this.http.post("http://localhost:8082/api/v1/property/save",bodyData,{responseType: 'text'}).subscribe((resultData: any)=>
+    {
+        console.log(resultData);
+        alert("Property Added Successfully");
+        this.getAllProperties();
+
+        this.propertyAddress = '';
+        this.propertyRentedStatus = '';
+        this.propertyContactNumber  = 0;
+    });
+  }
+  setUpdate(data: any)
+  {
+   this.propertyAddress = data.propertyAddress;
+   this.propertyRentedStatus = data.propertyRentedStatus;
+   this.propertyContactNumber = data.propertyContactNumber;
+   this.currentPropertyId = data.propertyId;
+  }
+
+
+  UpdateRecords()
+  {
+    let bodyData = {
+      "propertyId" : this.currentPropertyId,
+      "propertyAddress" : this.propertyAddress,
+      "propertyRentedStatus" : this.propertyRentedStatus,
+      "propertyContactNumber" : this.propertyContactNumber
+    };
+
+    this.http.put("http://localhost:8082/api/v1/property/update",bodyData,{responseType: 'text'}).subscribe((resultData: any)=>
+    {
+        console.log(resultData);
+        alert("Property Details Updated")
+        this.getAllProperties();
+        this.propertyAddress = '';
+        this.propertyRentedStatus = '';
+        this.propertyContactNumber  = 0;
+    });
+  }
+  save()
+  {
+    if(this.currentPropertyId == "")
+    {
+        this.register();
+    }
+      else
+      {
+       this.UpdateRecords();
+      }
+  }
+
+
+  setDelete(data: any)
+  {
+
+    this.http.delete("http://localhost:8082/api/v1/property/delete"+ "/"+ data.propertyId,{responseType: 'text'}).subscribe((resultData: any)=>
+    {
+        console.log(resultData);
+        alert("Property Deleted")
+        this.getAllProperties();
+
+        this.propertyAddress = '';
+        this.propertyRentedStatus = '';
+        this.propertyContactNumber  = 0;
+
+    });
+  }
 }
 
 
